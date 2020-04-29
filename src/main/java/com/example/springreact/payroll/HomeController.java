78ff01b66302136f3e31c892872b31d0f5f0e7ad
nate <nate.east@equilibriumtech.com>
2020-04-29 13:19:43 -0400

    Took sonarlint's suggestions

--- src/main/java/com/example/springreact/payroll/HomeController.java
+++ src/main/java/com/example/springreact/payroll/HomeController.java
@@ -1,12 +1,12 @@
 package com.example.springreact.payroll;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = "/")
+    @GetMapping(value = "/")
     public String index(){
         return "index";
     }
