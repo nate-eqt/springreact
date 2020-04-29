40cc04d2ae159f251706f91fe4cd1396bbda7bb0
nate <nate.east@equilibriumtech.com>
2020-04-29 15:42:56 -0400

    Completed part 1 of tutorial

---
+++ README.md
@@ -0,0 +1,7 @@
+Spring-React Accessibility Demo
+
+This project was created according to the tutorial found at:
+https://spring.io/guides/tutorials/react-and-spring-data-rest/
+
+The goal is to extend the tutorial project to be fully accessible via screen-reader and navigable using only the keyboard.
+
--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,3 +1,67 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const client = require('./client');
+
+class App extends React.Component {
+    /*
+    The common convention is to initialize state with all your attributes empty in the constructor.
+    Then you look up data from the server by using componentDidMount and populating your attributes.
+    From there on, updates can be driven by user action or other events.
+    */
+    constructor(props) {
+        super(props);
+        this.state = {employees: []};
+    }
+
+    componentDidMount(){
+        //Fetches the employee list and sets it into state
+        client({method: 'GET', path: '/api/employees'}).done(response => {
+            this.setState({employees: response.entity._embedded.employees});
+        });
+    }
+
+    render(){
+        return(<EmployeeList employees = {this.state.employees}/>)
+    }
+
+}
+
+class EmployeeList extends React.Component{
+    render(){
+        const employees = this.props.employees.map(employee =>
+            //Below is a demonstration of HATEOAS in action when setting the employee key
+            <Employee key={employee._links.self.href} employee={employee} />
+        );
+        return(
+            <table>
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Description</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {employees}
+                </tbody>
+            </table>
+        )
+    }
+}
+
+class Employee extends React.Component{
+    render(){
+        return (
+            <tr>
+                <td>{this.props.employee.firstName}</td>
+                <td>{this.props.employee.lastName}</td>
+                <td>{this.props.employee.description}</td>
+            </tr>
+        )
+    }
+}
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('react')
+)
